1937c88f2cf875477a22f861ebd0fa64404d1910
Sattwik <hellosattwik@gmail.com>
2024-01-04 15:47:58 +0530

    feat: Add script to seed users

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,23 +1,24 @@
-import { db } from '@/lib/db';
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker"
+
+import { db } from "@/lib/db"
 
 async function main() {
-  console.log('✨ Creating Users');
+  console.log("✨ Creating Users")
   await db.user.createMany({
     data: new Array(100).fill(0).map((_) => ({
       name: faker.person.fullName(),
       email: faker.internet.email(),
     })),
-  });
-  console.log('✅ Done');
+  })
+  console.log("✅ Done")
 }
 
 main()
   .then(async () => {
-    await db.$disconnect();
+    await db.$disconnect()
   })
   .catch(async (e) => {
-    console.error(e);
-    await db.$disconnect();
-    process.exit(1);
-  });
+    console.error(e)
+    await db.$disconnect()
+    process.exit(1)
+  })
