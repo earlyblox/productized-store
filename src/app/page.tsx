44d32a07f2812a912356feee939a7fccf90a45ea
Sattwik <hellosattwik@gmail.com>
2024-01-08 01:06:55 +0530

    chore(page): refactor page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
-import { SearchBar } from '@/components/search-bar';
-import { TopSearches } from '@/components/top-searchs';
-import { Container, Flex, Heading, Section, Text } from '@radix-ui/themes';
+import { ThemeToggle } from "@/components/theme-toggle"
+import { DiscoverPage } from "@/app/discover-page"
+
+import { Footer } from "./footer"
+import { Header } from "./header"
 
 export default function Home() {
   return (
-    <main className='min-h-screen'>
-      <Container>
-        <Section>
-          <Flex direction='column' gap='3'>
-            <Heading as='h1' align='center' size='9'>
-              Discover Productized Services
-            </Heading>
-            <Text align='center' size='5'>
-              Explore the best productized services in the market.
-            </Text>
-          </Flex>
-          <SearchBar />
-          <TopSearches />
-        </Section>
-      </Container>
+    <main className="relative min-h-screen">
+      <Header />
+      <DiscoverPage />
+      <Footer />
     </main>
-  );
+  )
 }
